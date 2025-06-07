46dc4edf78fe45d9e85a448c99bbdfa1a4fc2ed9
FarhanABD <farhanabdilah204@gmail.com>
2025-06-07 07:56:10 +0700

    datetime helper

---
+++ lib/core/dependency-injection/dependency.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+final sl = GetIt.instance;
+
+void initDependency() {
+  // dio
+  Dio dio = Dio();
+  dio.interceptors.add(
+    PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      compact: false,
+    ),
+  );
+  sl.registerSingleton<Dio>(dio);
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kasir/core/dependency-injection/dependency.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id', null);
+  initDependency();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.red),
+      home: Scaffold(body: Center(child: Text('Hello World'))),
+    );
+  }
+}
